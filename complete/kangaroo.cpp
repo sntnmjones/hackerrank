cf9cb563d800764a8405b9a98d006053a05535d9
Troy Jones <sntnmjones@gmail.com>
2018-11-02 11:12:21 -0700

    kangaroo complete

--- complete/kangaroo.cpp
+++ complete/kangaroo.cpp
@@ -23,7 +23,9 @@ string kangaroo(int x1, int v1, int x2, int v2)
     }
     else
     {
-        if(x1 % v2 == 0 || x2 % v1 == 0)
+        // If the difference between position is evenly divisible by the difference in
+        //      hop distance.
+        if(((x1 - x2) % (v2 - v1)) == 0)
         {
             return "YES";
         }
